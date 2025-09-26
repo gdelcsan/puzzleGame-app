f17bf0646de2df3d830c38e0081ed95b3c66a20e
Gaby <gabrieladelcristo@gmail.com>
2025-09-26 16:46:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,7 +172,7 @@ if "start_time" not in st.session_state:
     st.session_state.start_time = None
 
 # Upload
-uploaded = st.file_uploader(type=["png","jpg","jpeg"])
+uploaded = st.file_uploader("", type=["png","jpg","jpeg"])
 if uploaded is not None:
     file_bytes = uploaded.getvalue()
     upload_fingerprint = (uploaded.name, len(file_bytes), hashlib.md5(file_bytes).hexdigest()) 
