a4a870c5f71f7f4a7d36d1639a14f47afc61fe13
Gaby <gabrieladelcristo@gmail.com>
2025-10-21 16:16:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -187,16 +187,16 @@ with tab1:
       return tiles
 
     # Session state initialization
-    if "tiles" not in st.session_state:
-      default = Image.new("RGB",(450,450),(21, 109, 153))
-      draw = ImageDraw.Draw(default)
-      st.session_state.tiles = slice_into_tiles(default)
-    if "history" not in st.session_state:
-      st.session_state.history = []
-    if "auto_play" not in st.session_state:
-      st.session_state.auto_play = False
-    if "start_time" not in st.session_state:
-      st.session_state.start_time = None
+      if "tiles" not in st.session_state:
+        default = Image.new("RGB",(450,450),(21, 109, 153))
+        draw = ImageDraw.Draw(default)
+        st.session_state.tiles = slice_into_tiles(default)
+      if "history" not in st.session_state:
+        st.session_state.history = []
+      if "auto_play" not in st.session_state:
+        st.session_state.auto_play = False
+      if "start_time" not in st.session_state:
+        st.session_state.start_time = None
 
   # Upload
   uploaded = st.file_uploader("", type=["png","jpg","jpeg"])
