f9c11353c9328e68ae6e7669ee9776f3085ba1c8
Gaby <gabrieladelcristo@gmail.com>
2025-10-21 16:28:51 -0400

    Refactor file uploader logic for image processing

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,21 +204,21 @@ with tab1:
         st.session_state.start_time = None
 
   # Upload
-          uploaded = st.file_uploader("", type=["png","jpg","jpeg"])
-            if uploaded is not None:
-                file_bytes = uploaded.getvalue()
-                upload_fingerprint = (uploaded.name, len(file_bytes), hashlib.md5(file_bytes).hexdigest()) 
-            if st.session_state.last_upload_id != upload_fingerprint: #initializing only for a new file upload
-                img = Image.open(BytesIO(file_bytes)).convert("RGB")
-                st.session_state.tiles = slice_into_tiles(img)
-                st.session_state.state = GOAL
-                st.session_state.solution = None
-                st.session_state.sol_index = 0
-                st.session_state.last_upload_id = upload_fingerprint
-                st.success("Image loaded and sliced. Start shuffling or play from the goal!")
+  uploaded = st.file_uploader("", type=["png","jpg","jpeg"])
+    if uploaded is not None:
+        file_bytes = uploaded.getvalue()
+        upload_fingerprint = (uploaded.name, len(file_bytes), hashlib.md5(file_bytes).hexdigest()) 
+    if st.session_state.last_upload_id != upload_fingerprint: #initializing only for a new file upload
+        img = Image.open(BytesIO(file_bytes)).convert("RGB")
+            st.session_state.tiles = slice_into_tiles(img)
+            st.session_state.state = GOAL
+            st.session_state.solution = None
+            st.session_state.sol_index = 0
+            st.session_state.last_upload_id = upload_fingerprint
+            st.success("Image loaded and sliced. Start shuffling or play from the goal!")
 
   # Controls
-col1, col2, col3, col4 = st.columns([1,1,1,1])
+  col1, col2, col3, col4 = st.columns([1,1,1,1])
     with col1:
     if st.sidebar.button("Shuffle pieces", key="btn_shuffle"):
         # perform many random legal moves from GOAL to guarantee solvable
